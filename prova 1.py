ba3524ce2f138fa615b2acdad48d10e7d9b77473
gabrielmarianodeoliveira0-hue <gabrielmarianodeoliveira0@gmail.com>
2026-05-08 16:39:31 -0300

    Add files via upload

---
+++ prova 1.py
@@ -0,0 +1,33 @@
+while True:
+    #prova para calcular o imc
+    nome = input("Digite seu nome:")
+    idade = int(input("Digite sua idade(anos):"))
+    peso = float(input("Digite seu peso (kg): "))
+    altura = float(input("Digite sua altura (m): "))
+    #calcular o IMC do paciente
+    imc = peso / (altura ** 2)
+    print(f"nome:{nome}")
+    print(f"idade:{idade}")
+    print(f"peso:{peso}")
+    print(f"altura:{altura}")    
+            #classificação do imc
+    if imc < 18.5:
+        print("Abaixo do peso 💪")
+    elif imc < 24.9:
+        print("Peso normal 💪")
+    elif imc < 29.9:
+        print("Sobrepeso 🍔")
+    elif imc < 34.9:
+        print("Obesidade Grau I 🟠")
+    elif imc < 39.9:
+        print("Obesidade Grau II 🍔")
+    elif imc >= 40.0:          
+        print("Obesidade Grau III (mórbida) 🚨")
+    else:         
+        print("gordão")
+    continuar = input("deseja calcular o próximo IMC?  (s/n):").lower()
+    if continuar !="s":
+       print("voce decidiu sair do programa")
+       break
+      
+            
